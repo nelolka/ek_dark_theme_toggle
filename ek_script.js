5869ac73682bc1e1392a5b8573f770e6a16fe15f
nelolka <imnelolka@gmail.com>
2025-09-25 15:29:10 +0300

    Update ek_script.js
    update and homepage url

--- ek_script.js
+++ ek_script.js
@@ -4,6 +4,11 @@
 // @version      1.0
 // @description  Автоматически переключает темную тему без обновления страницы
 // @match        https://ek.ua/*
+// @updateURL    https://raw.githubusercontent.com/nelolka/ek_dark_theme_toggle/refs/heads/main/ek_script.js
+// @downloadURL  https://raw.githubusercontent.com/nelolka/ek_dark_theme_toggle/refs/heads/main/ek_script.js
+// @homepageURL  https://github.com/nelolka/ek_dark_theme_toggle/
+// @supportURL   https://github.com/nelolka/ek_dark_theme_toggle/issues/
+// @source       https://github.com/nelolka/ek_dark_theme_toggle/
 // @run-at       document-start
 // ==/UserScript==
 
